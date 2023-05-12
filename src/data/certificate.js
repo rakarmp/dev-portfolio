589222d49f4883882295ca12fb900bd2a8ebec0b
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-05-12 18:18:35 +0700

    fix path img

--- src/data/certificate.js
+++ src/data/certificate.js
@@ -3,6 +3,6 @@ export default [
     title: "React JS",
     imgUrl: "/assets/certificate/great-learning.png",
     stack: ["By Greate Learning"],
-    link: "https://blog-daily.netlify.app/",
+    link: "https://raw.githubusercontent.com/rakarmp/dev-portfolio/master/public/assets/certificate/great-learning.png",
   },
 ];
