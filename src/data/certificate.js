0e07f8f75dbbb64ac56e94ad02efffc2571f367b
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-05-12 22:21:55 +0700

    add new certificate

--- src/data/certificate.js
+++ src/data/certificate.js
@@ -2,7 +2,13 @@ export default [
   {
     title: "React JS",
     imgUrl: "/assets/certificate/great-learning.png",
-    stack: ["By Greate Learning"],
+    stack: ["Issued By Greate Learning"],
     link: "https://raw.githubusercontent.com/rakarmp/dev-portfolio/master/public/assets/certificate/great-learning.png",
+  },
+  {
+    title: "Baparekraf Developer Day 2023",
+    imgUrl: "/assets/certificate/dicoding.png",
+    stack: ["Issued By Dicoding"],
+    link: "https://raw.githubusercontent.com/rakarmp/dev-portfolio/master/public/assets/certificate/dicoding.png",
   },
 ];
